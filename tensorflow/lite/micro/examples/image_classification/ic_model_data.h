788f1788f64311e3de68ce1ea4215db0fcf54c2a
Mahmoud Abumandour <mam47@sfu.ca>
2023-08-23 11:21:49 -0700

    Reload IC model

--- tensorflow/lite/micro/examples/image_classification/ic_model_data.h
+++ tensorflow/lite/micro/examples/image_classification/ic_model_data.h
@@ -1,22 +1,4 @@
-/* Copyright 2020 The TensorFlow Authors. All Rights Reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-
-#ifndef V0_1_IC_IC_MODEL_DATA_H_
-#define V0_1_IC_IC_MODEL_DATA_H_
-
-alignas(16) const unsigned char pretrainedResnet_quant_tflite[] = {
+unsigned char pretrainedResnet_quant_tflite[] = {
   0x1c, 0x00, 0x00, 0x00, 0x54, 0x46, 0x4c, 0x33, 0x00, 0x00, 0x12, 0x00,
   0x1c, 0x00, 0x18, 0x00, 0x14, 0x00, 0x10, 0x00, 0x0c, 0x00, 0x08, 0x00,
   0x00, 0x00, 0x04, 0x00, 0x12, 0x00, 0x00, 0x00, 0x18, 0x00, 0x00, 0x00,
@@ -8226,6 +8208,4 @@ alignas(16) const unsigned char pretrainedResnet_quant_tflite[] = {
   0x0f, 0x00, 0x00, 0x00, 0x08, 0x00, 0x04, 0x00, 0x0c, 0x00, 0x00, 0x00,
   0x03, 0x00, 0x00, 0x00, 0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x03
 };
-const unsigned int pretrainedResnet_quant_tflite_len = 98496;
-
-#endif  // V0_1_IC_IC_MODEL_DATA_H_
+unsigned int pretrainedResnet_quant_tflite_len = 98496;
